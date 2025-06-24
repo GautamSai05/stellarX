f87fe9fa7ae67ebde379417898e88461f282d39f
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-06-24 18:20:05 +0530

    cred

--- StellarSense/settings.py
+++ StellarSense/settings.py
@@ -80,10 +80,10 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stellar',
-        'USER': 'stellaruser',
-        'PASSWORD': 'stellarpwd',
-        'HOST': 'localhost',
+        'NAME': 'stellar_db_d0rf'
+        'USER': 'stellar_db_d0rf_user',
+        'PASSWORD': '6NbQh81nqiwmuxyuR8hE0evTWgUc3B8X',
+        'HOST': 'dpg-d1d9se6r433s73f1c8jg-a',
         'PORT': '5432',
     }
 }
