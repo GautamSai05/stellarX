2c8f615214220b922d5ba94b6baacd991999fe14
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-06-29 14:23:11 +0530

    remove grpah

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<str:username>/', views.profile_view, name='profile'),
-    path('<str:username>/<int:year>/', views.profile_view, name='profile_by_year'),
+    
 ]
