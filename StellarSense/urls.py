d57fa2fe80391944184e12afb0134b58c8f6930f
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-07-02 10:49:13 +0530

    Fix: Ensure all colors are black, white, or light black and fix logout redirect

--- StellarSense/urls.py
+++ StellarSense/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 
     # Auth
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('accounts/signup/', signup_view, name='signup'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
