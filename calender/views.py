27a01e9a89b13974271e728d0d8c55f96d5ebbc2
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-07-03 16:17:13 +0530

    model switch

--- calender/views.py
+++ calender/views.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 # Load .env variables
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-gemini_model = genai.GenerativeModel("models/gemini-1.5-flash")
+gemini_model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 def calendar_chatbot(request):
     if request.method == 'POST':
