7d01b61c92cec74a61cfece5df2b8a76a75c7ec7
GautamSai05 <goutam300505@gmail.com>
2025-10-03 23:36:55 +0530

    Revert "feat: Implement space weather alerts and user profiles (#1)"
    This reverts commit afdab48f9bc5c52368fdf0aec0830c6c89a61e88.

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('settings/', views.profile_settings_view, name='profile_settings'),
     path('<str:username>/', views.profile_view, name='profile'),
+    
 ]
